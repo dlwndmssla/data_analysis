8441870c805029cc2fbcac64c39be2d1b9bf7e0b
dlwndmssla <dlwndmssla@gmail.com>
2023-09-04 10:52:56 +0900

    기타

---
+++ first_copy5.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sklearn.preprocessing
+from datetime import datetime
+
+now0 = datetime.now()
+################################################################
+
+
+
+
+
+
+
+
+################################################################
+now = datetime.now()
+print()
+print(now - now0 ,'초 실행')
+print("현재 : ", now)
